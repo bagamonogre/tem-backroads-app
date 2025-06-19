8e21d493ca7d1f40da5f2048efbc9b581fdc7eb0
wen <y82sfjm@gmail.com>
2025-06-20 00:17:07 +0800

    first commit

---
+++ src/components/Services.js
@@ -0,0 +1,28 @@
+import { services } from "../data";
+import Title from "./Title";
+
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subtitle="services" />
+      <div className="section-center services-center">
+        {services.map((link) => {
+          const { icon, title, text } = link;
+          return (
+            <article className="service">
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{title}</h4>
+                <p className="service-text">{text}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
---
+++ src/data.js
@@ -0,0 +1,103 @@
+import tourImg1 from "./images/tour-1.jpeg";
+import tourImg2 from "./images/tour-2.jpeg";
+import tourImg3 from "./images/tour-3.jpeg";
+import tourImg4 from "./images/tour-4.jpeg";
+import tourImg5 from "./images/tour-5.jpeg";
+import tourImg6 from "./images/tour-6.jpeg";
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tour", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tourImg1,
+    date: "august 26th, 2020",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    title: "Tibet Adventure",
+    location: "Tibet Adventure",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tourImg2,
+    date: "august 26th, 2020",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    title: "Tibet Adventure",
+    location: "Tibet Adventure",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 3,
+    image: tourImg3,
+    date: "august 26th, 2020",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    title: "Tibet Adventure",
+    location: "Tibet Adventure",
+    duration: "6 days",
+    cost: 2100,
+  },
+  {
+    id: 4,
+    image: tourImg4,
+    date: "august 26th, 2020",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    title: "Tibet Adventure",
+    location: "Tibet Adventure",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 5,
+    image: tourImg5,
+    date: "august 26th, 2020",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    title: "Tibet Adventure",
+    location: "Tibet Adventure",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 6,
+    image: tourImg6,
+    date: "august 26th, 2020",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    title: "Tibet Adventure",
+    location: "Tibet Adventure",
+    duration: 6,
+    cost: 2100,
+  },
+];
